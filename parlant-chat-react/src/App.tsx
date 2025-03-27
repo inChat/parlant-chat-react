c9a5775db5c2d2bc89f148643f7faea89b85ad56
Menachem Brichta <menachem@emcie.co>
2025-03-27 11:32:41 +0200

    feat(chatbot): add custom popup button and classNames support
    Extend the Chatbot component to allow custom popup buttons and additional classNames for default popup button and icon. This provides more flexibility for styling and customization.

--- parlant-chat-react/src/App.tsx
+++ parlant-chat-react/src/App.tsx
@@ -1,9 +1,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Chat from './components/chat/chat';
-import { ClassNameValue } from 'tailwind-merge';
+import { ClassNameValue, twMerge } from 'tailwind-merge';
 import './App.css'
 import './index.css'
-import { JSX, useRef, useState } from 'react';
+import { JSX, ReactElement, useRef, useState } from 'react';
 import { Button } from './components/ui/button';
 import { MessageSquare, X } from 'lucide-react';
 // import './parlant-chat-react.css'
@@ -13,18 +13,21 @@ export interface ChatProps {
   route: string;
   sessionId: string;
   asPopup?: boolean;
+  popupButton?: JSX.Element;
   classNames?: {
       chatbox?: ClassNameValue;
       messagesArea?: ClassNameValue;
       agentMessage?: ClassNameValue;
       customerMessage?: ClassNameValue;
       textarea?: ClassNameValue;
+      defaultPopupButton?: ClassNameValue;
+      defaultPopupButtonIcon?: ClassNameValue;
   }
 }
 
 const queryClient = new QueryClient();
 
-export const Chatbot = ({route, sessionId, asPopup, classNames = {}}: ChatProps): JSX.Element => {
+export const Chatbot = ({route, sessionId, asPopup, popupButton, classNames = {}}: ChatProps): JSX.Element => {
   const buttonRef = useRef<HTMLButtonElement>(null);
   const [chatOpen, setChatOpen] = useState(false);
   const rect = buttonRef?.current?.getBoundingClientRect();
@@ -34,8 +37,8 @@ export const Chatbot = ({route, sessionId, asPopup, classNames = {}}: ChatProps)
       <QueryClientProvider client={queryClient}>
         {asPopup ? (
           <>
-            <Button ref={buttonRef} onClick={() => setChatOpen(val => !val)} className="bg-black rounded-full size-[50px] p-[10px] m-[10px]">
-              {<Icon size={30} color='white' className='min-w-[30px] min-h-[30px]'/>}
+            <Button ref={buttonRef} onClick={() => setChatOpen(val => !val)} className={twMerge("bg-black rounded-full min-w-fit size-[50px] p-[10px] m-[10px]", classNames?.defaultPopupButton)}>
+              {popupButton || <Icon size={30} color='white' className={twMerge('min-w-[30px] min-h-[30px]', classNames?.defaultPopupButtonIcon)}/>}
             </Button>
             {chatOpen && <div className='fixed !-translate-[100%]' style={{top: `${(rect?.y || 0)}px`, left: `${rect?.x}px`}}><Chat route={route} sessionId={sessionId} classNames={classNames}/></div>}
           </>
